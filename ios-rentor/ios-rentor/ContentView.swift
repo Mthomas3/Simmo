f4a518bb2c6ca85350af3469d70a680bd78e8876
mthomas3 <thomas.martins.dev@gmail.com>
2020-08-29 16:41:40 +0200

    [init] project

---
+++ ios-rentor/ios-rentor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-rentor
+//
+//  Created by Thomas on 29/08/2020.
+//  Copyright © 2020 Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-rentor/ios-rentorTests/ios_rentorTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_rentorTests.swift
+//  ios-rentorTests
+//
+//  Created by Thomas on 29/08/2020.
+//  Copyright © 2020 Thomas. All rights reserved.
+//
+
+import XCTest
+@testable import ios_rentor
+
+class ios_rentorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
