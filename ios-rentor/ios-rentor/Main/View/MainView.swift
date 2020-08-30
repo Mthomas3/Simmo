8afc61b628bf4ca14d54cfefc87768518dc7a2f5
mthomas3 <thomas.martins.dev@gmail.com>
2020-08-30 15:08:26 +0200

    re work on custom bar view... 🎉

--- ios-rentor/ios-rentor/Main/View/MainView.swift
+++ ios-rentor/ios-rentor/Main/View/MainView.swift
@@ -9,19 +9,64 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @State var index = 0
     var body: some View {
-        TabView {
-            SimmulatorView()
-            .tabItem {
-                Image("sliders")
-                Text("Simulations")
-            }
-            SettingView()
-            .tabItem {
-                Image("settings")
-                Text("Settings")
-            }
+        VStack {
+            Spacer()
+            CustomTabs(index: self.$index)
         }
+        .background(Color.black.opacity(0.04).edgesIgnoringSafeArea(.top))
+    }
+}
+
+struct CustomTabs: View {
+    
+    @Binding var index: Int
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                self.index = 0
+                
+            }, label: {Image("sliders")})
+                .foregroundColor(Color.black.opacity(self.index == 0 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                self.index = 1
+                
+            }, label: {Image("settings")})
+                .foregroundColor(Color.black.opacity(self.index == 1 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {},
+                   label: { Image("add-circle").renderingMode(.original)})
+                .offset(y: -20)
+                
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                self.index = 2
+
+            }, label: {Image("sliders")})
+                .foregroundColor(Color.black.opacity(self.index == 2 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                   self.index = 2
+
+               }, label: {Image("sliders")})
+                   .foregroundColor(Color.black.opacity(self.index == 2 ? 1 : 0.2))
+            
+        }
+        .padding(.horizontal, 35)
+        .padding(.vertical, 5)
+        .background(Color.white)
     }
 }
 
