14c373306df484e22a4d78596fcec07d4d9c0820
mthomas3 <thomas.martins.dev@gmail.com>
2020-09-29 17:40:38 +0200

    sim view is almost done

--- ios-rentor/ios-rentor/Simmulator/View/SimmulatorView.swift
+++ ios-rentor/ios-rentor/Simmulator/View/SimmulatorView.swift
@@ -84,19 +84,3 @@ internal struct SimmulatorView: View {
         SimmulatorCellView(with: name, and: cell, with: self.refreshEvent)
     }
 }
-
-
-struct BlueButtonStyle: ButtonStyle {
-    
-    var disabled: Bool = false
-
-  func makeBody(configuration: Self.Configuration) -> some View {
-    return configuration.label
-        .font(.headline)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        .contentShape(Rectangle())
-        .background(configuration.isPressed ? Color.blue.opacity(0.5) : (disabled ? Color.blue.opacity(0.5) : Color.blue))
-        .foregroundColor(configuration.isPressed ? Color.white.opacity(0.5) : Color.white)
-        .cornerRadius(8)
-  }
-}
