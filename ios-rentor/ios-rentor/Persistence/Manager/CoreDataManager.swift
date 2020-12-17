acc085abeb42409bd82cf9d96c5c9a79465f8c25
tom <thomas.martins.dev@gmail.com>
2020-12-17 20:34:41 +0100

    [ADD] promise on request 🎄

---
+++ ios-rentor/ios-rentor/Persistence/Manager/CoreDataManager.swift
@@ -0,0 +1,68 @@
+//
+//  CoreDataPresenter.swift
+//  ios-rentor
+//
+//  Created by Thomas on 05/09/2020.
+//  Copyright © 2020 Thomas. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+import Combine
+
+internal final class CoreDataManager<Entity> where Entity: NSManagedObject {
+    private let request: NSFetchRequest<Entity>
+    private let context: NSManagedObjectContext
+    private let subject: PassthroughSubject<Entity, Never>
+    
+    init(request: NSFetchRequest<Entity>, context: NSManagedObjectContext) {
+        if request.sortDescriptors == nil {
+            request.sortDescriptors = []
+        }
+        self.request = request
+        self.context = context
+        self.subject = PassthroughSubject<Entity, Never>()
+    }
+    
+    internal func fetch() -> AnyPublisher<[Entity], CoreDataError> {
+        Future<[Entity], CoreDataError> {
+            do {
+                $0(.success(try self.context.fetch(self.request)))
+            } catch {
+                $0(.failure(.fetchError))
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    internal func onUpdate() -> AnyPublisher<Entity, Never> {
+        return self.subject.eraseToAnyPublisher()
+    }
+    
+    internal func create(with item: Entity) -> AnyPublisher<Void, CoreDataError> {
+        Future<Void, CoreDataError> {
+            do {
+                self.context.insert(item)
+                self.subject.send(item)
+                $0(.success(try self.context.save() as Void))
+            } catch {
+                $0(.failure(.createError))
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    internal func delete(with item: Entity) throws {
+        self.context.delete(item)
+        try self.context.save()
+    }
+    
+    internal func deleteOn(with data: Entity) -> AnyPublisher<Void, CoreDataError> {
+        Future<Void, CoreDataError> {
+            do {
+                $0(.success(try self.context.save() as Void))
+            } catch {
+                $0(.failure(.deleteError))
+            }
+        }.eraseToAnyPublisher()
+    }
+}
