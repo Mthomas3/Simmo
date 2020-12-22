4417ea36d36de96c3beb548cbf43260afa84bce0
tom <thomas.martins.dev@gmail.com>
2020-12-22 22:57:12 +0100

    [UPDATE] work on models 🎉

--- ios-rentor/ios-rentor/Models/MockedData.swift
+++ ios-rentor/ios-rentor/Models/MockedData.swift
@@ -12,10 +12,12 @@ import Foundation
 
 extension Rentor {
     static let mockedData: [Rentor] = [
-        Rentor(date: Date(), name: "Flat Simmulation", price: 120000000, rentPrice: 2500, cashFlow: 750, percentage: 5.0),
-        Rentor(date: Date(), name: "House Simmulation", price: 250000000, rentPrice: 4500, cashFlow: 1750, percentage: 10.0),
-        Rentor(date: Date(), name: "Flat Simmulation", price: 250000000, rentPrice: 4500, cashFlow: 750, percentage: 3.0),
+        Rentor(date: Date(), name: "Flat Simmulation",
+               price: 120000, rentPrice: 2500, cashFlow: 750, percentage: 5.0),
+        Rentor(date: Date(), name: "House Simmulation",
+               price: 250000, rentPrice: 4500, cashFlow: 1750, percentage: 10.0),
+        Rentor(date: Date(),
+               name: "Flat Simmulation", price: 250000, rentPrice: 4500, cashFlow: 750, percentage: 3.0)
     ]
 }
-
 #endif
