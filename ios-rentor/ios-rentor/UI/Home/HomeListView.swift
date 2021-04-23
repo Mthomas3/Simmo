f547016d27e90e445a7a7bdc8e99d1d40f2eb391
mthomas3 <thomas.martins@soprasteria.com>
2021-04-23 16:46:39 +0200

    [TAB BAR] image

--- ios-rentor/ios-rentor/UI/Home/HomeListView.swift
+++ ios-rentor/ios-rentor/UI/Home/HomeListView.swift
@@ -17,7 +17,8 @@ struct HomeListView: View {
     @State private var showingAddForm = false
     @State private var showCancelButton: Bool = false
     @State private var searchText = ""
-    
+    @ObservedObject var searchBar: SearchBar = SearchBar()
+
     var body: some View {
         NavigationView {
             /*VStack {
@@ -70,6 +71,7 @@ struct HomeListView: View {
          }*/propertyList
             .navigationBarTitle(Text(self.navigationBarTitle))
             .navigationBarItems(trailing: addButton)
+            .add(self.searchBar)
         }
     }
 }
@@ -79,73 +81,73 @@ extension HomeListView {
     var propertyList: some View {
         Group {
             if store.state.homeState.homeRentors.count > 0 {
-                VStack(spacing: 0) {
-                    HStack {
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                            TextField("search", text: $searchText, onEditingChanged: { _ in
-                                self.showCancelButton = true
-                            }, onCommit: {
-                                print("onCommit")
-                            }).foregroundColor(.primary)
-
-                            Button(action: {
-                                self.searchText = ""
-                            }) {
-                                Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
-                            }
-                        }.padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
-                        .foregroundColor(.secondary)
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(10.0)
-                        if showCancelButton {
-                            Button("Cancel") {
-                                UIApplication.shared.endEditing(true)
-                                self.searchText = ""
-                                self.showCancelButton = false
-                            }
-                            .foregroundColor(Color(.systemBlue))
-                        }
-                    }.padding(.horizontal)
-                    .navigationBarHidden(showCancelButton)
-                    .background(Color.blue)
-                    List {
-                        ForEach(store.state.homeState.homeRentors.filter { ($0.name ?? "")
-                                    .hasPrefix(searchText) || searchText == ""}) { property in
-                            ZStack {
-                                HomeRowView(rentor: property)
-                                    .padding(.top, 10)
-                                    .background(Color.pink)
-                                    
-                                NavigationLink(destination: HomeDetailView(with: property)) {
-                                    EmptyView()
-                                        .background(Color.gray)
-                                }.frame(width: 0)
-                                .opacity(0)
-                                //.buttonStyle(PlainButtonStyle())
+                List {
+                    ForEach(store.state.homeState.homeRentors.filter { ($0.name ?? "")
+                                .hasPrefix(searchText) || searchText == ""}) { property in
+                        /*ZStack {
+                            HomeRowView(rentor: property)
                                 .padding(.top, 10)
-                                .background(Color.black)
-                                
-                            }.listRowInsets(EdgeInsets())
-                            .padding(.all, 8)
-                            .listRowBackground(Color.init("TableViewGray"))
-                            .background(Color.green)
+                                .background(Color.pink)
                             
-                        }.onDelete(perform: onDelete)
-                    }.listStyle(GroupedListStyle())
-                    .resignKeyboardOnDragGesture()
-                    .padding(.top, 10)
-                    .background(Color.yellow)
-                }.background(Color.red)
+                            NavigationLink(destination: HomeDetailView(with: property)) {
+                                EmptyView()
+                                    .background(Color.gray)
+                            }.frame(width: 0)
+                            .opacity(0)
+                            //.buttonStyle(PlainButtonStyle())
+                            .padding(.top, 10)
+                            .background(Color.black)
+                            
+                        }.listRowInsets(EdgeInsets())
+                        .padding(.all, 8)
+                        .listRowBackground(Color.init("TableViewGray"))
+                        .background(Color.green)*/
+                        
+                        //HomeRowView(rentor: property)
+                        StoreRow(title: property.name ?? "", address: "MDR?", city: "TOTO", categories: ["NOTHING"], kilometres: 30)
+                        
+                    }.onDelete(perform: onDelete)
+                }
+                /*List {
+                    ForEach(store.state.homeState.homeRentors.filter { ($0.name ?? "")
+                                .hasPrefix(searchText) || searchText == ""}) { property in
+                        /*ZStack {
+                            HomeRowView(rentor: property)
+                                .padding(.top, 10)
+                                .background(Color.pink)
+                            
+                            NavigationLink(destination: HomeDetailView(with: property)) {
+                                EmptyView()
+                                    .background(Color.gray)
+                            }.frame(width: 0)
+                            .opacity(0)
+                            //.buttonStyle(PlainButtonStyle())
+                            .padding(.top, 10)
+                            .background(Color.black)
+                            
+                        }.listRowInsets(EdgeInsets())
+                        .padding(.all, 8)
+                        .listRowBackground(Color.init("TableViewGray"))
+                        .background(Color.green)*/
+                        
+                        //HomeRowView(rentor: property)
+                        StoreRow(title: property.name ?? "", address: "MDR?", city: "TOTO", categories: ["NOTHING"], kilometres: 30)
+                            .listRowBackground(Color.black)
+                    }.onDelete(perform: onDelete)
+                }.resignKeyboardOnDragGesture()*/
             } else {
                 Text("Please add a property... \(store.state.homeState.homeRentors.count)")
             }
-        }
+        }.padding(.all, -20)
+        
     }
     
     var addButton: some View {
         Group {
-            Button(action: { showingAddForm.toggle() }) {
+            Button(action: {
+                //showingAddForm.toggle()
+                store.dispatch(.settingsAction(action: .setHasLaunchedApp(status: false)))
+            }) {
                 Image(systemName: "plus")
                     .imageScale(.large)
                     .foregroundColor(Color.init("PrimaryViolet"))
@@ -163,26 +165,68 @@ struct HomeListView_Previews: PreviewProvider {
     }
 }
 
-extension UIApplication {
-    func endEditing(_ force: Bool) {
-        self.windows
-            .filter{$0.isKeyWindow}
-            .first?
-            .endEditing(force)
-    }
-}
-
-struct ResignKeyboardOnDragGesture: ViewModifier {
-    var gesture = DragGesture().onChanged{_ in
-        UIApplication.shared.endEditing(true)
-    }
-    func body(content: Content) -> some View {
-        content.gesture(gesture)
-    }
-}
-
-extension View {
-    func resignKeyboardOnDragGesture() -> some View {
-        return modifier(ResignKeyboardOnDragGesture())
+struct StoreRow: View {
+    
+    var title: String
+    var address: String
+    var city: String
+    var categories: [String]
+    var kilometres: Double
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            
+            Color.yellow
+            HStack {
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.red, .blue]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                    
+                    VStack {
+                        Text("\(kilometres)")
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundColor(.white)
+                        
+                        Text("km")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                }
+                .frame(width: 70, height: 70, alignment: .center)
+                
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .lineLimit(2)
+                        .padding(.bottom, 5)
+                    
+                    Text(address)
+                        .padding(.bottom, 5)
+                    
+                    HStack(alignment: .center) {
+                        Image(systemName: "mappin")
+                        Text(city)
+                    }
+                    .padding(.bottom, 5)
+                    
+                    HStack {
+                        ForEach(categories, id: \.self) { category in
+                            //CategoryPill(categoryName: category)
+                        }
+                    }
+                    
+                }
+                .padding(.horizontal, 5)
+            }
+            .padding(15)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
---
+++ ios-rentor/ios-rentor/UI/Home/SearchBar/SearchBar.swift
@@ -0,0 +1,52 @@
+//
+//  SearchBar.swift
+//  ios-rentor
+//
+//  Created by Thomas on 23/04/2021.
+//  Copyright © 2021 Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+class SearchBar: NSObject, ObservableObject {
+    
+    @Published var text: String = ""
+    let searchController: UISearchController = UISearchController(searchResultsController: nil)
+    
+    override init() {
+        super.init()
+        self.searchController.obscuresBackgroundDuringPresentation = false
+        self.searchController.searchResultsUpdater = self
+    }
+}
+
+extension SearchBar: UISearchResultsUpdating {
+   
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchBarText = searchController.searchBar.text {
+            self.text = searchBarText
+        }
+    }
+}
+
+struct SearchBarModifier: ViewModifier {
+    
+    let searchBar: SearchBar
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                ViewControllerResolver { viewController in
+                    viewController.navigationItem.searchController = self.searchBar.searchController
+                }
+                    .frame(width: 0, height: 0)
+            )
+    }
+}
+
+extension View {
+    
+    func add(_ searchBar: SearchBar) -> some View {
+        return self.modifier(SearchBarModifier(searchBar: searchBar))
+    }
+}
