25474382bf87f0f1009c058b2615c346fe122663
mthomas3 <thomas.martins@soprasteria.com>
2021-04-23 14:20:56 +0200

    BEFORE rework on card

--- ios-rentor/ios-rentor/UI/Home/HomeListView.swift
+++ ios-rentor/ios-rentor/UI/Home/HomeListView.swift
@@ -10,65 +10,135 @@ import SwiftUI
 
 struct HomeListView: View {
     
-    private let navigationBarTitle: String = "Home 🏡"
-    @EnvironmentObject private var store: AppStore
-    
     public let onDelete: (IndexSet) -> Void
     
+    private let navigationBarTitle: String = "Mes Simmulations"
+    @EnvironmentObject private var store: AppStore
     @State private var showingAddForm = false
+    @State private var showCancelButton: Bool = false
+    @State private var searchText = ""
     
     var body: some View {
         NavigationView {
-            propertyList
-                .navigationBarTitle(Text(self.navigationBarTitle))
-                .navigationBarItems(trailing: HStack { addButton
-                    testButton
-                })
-         }
+            /*VStack {
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        TextField("search", text: $searchText, onEditingChanged: { _ in
+                            self.showCancelButton = true
+                        }, onCommit: {
+                            print("onCommit")
+                        }).foregroundColor(.primary)
+
+                        Button(action: {
+                            self.searchText = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                        }
+                    }.padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                    .foregroundColor(.secondary)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10.0)
+                    if showCancelButton {
+                        Button("Cancel") {
+                            UIApplication.shared.endEditing(true)
+                            self.searchText = ""
+                            self.showCancelButton = false
+                        }
+                        .foregroundColor(Color(.systemBlue))
+                    }
+                }.padding(.horizontal)
+                .navigationBarHidden(showCancelButton)
+//
+//                propertyList
+//                    .navigationBarTitle(Text(self.navigationBarTitle))
+//                    .navigationBarItems(trailing: addButton)
+
+
+                List {
+                    
+                    ForEach(store.state.homeState.homeRentors.filter { ($0.name ?? "").hasPrefix(searchText) || searchText == ""}) {
+                        searchText in Text(searchText.name ?? "")
+                    }
+                    /*ForEach(store.state.homeState.homeRentors.filter{ $0.hasPrefix(searchText) || searchText == ""}, id:\.self) {
+                        searchText in Text(searchText)
+                    }*/
+                }
+                .navigationBarTitle(Text("Search"))
+                .resignKeyboardOnDragGesture()
+            }
+         }*/propertyList
+            .navigationBarTitle(Text(self.navigationBarTitle))
+            .navigationBarItems(trailing: addButton)
+        }
     }
 }
 
 extension HomeListView {
     
-    var headerView: some View {
-        Text("\(store.state.homeState.headerTitle)$US / month 💵")
-    }
-    
     var propertyList: some View {
         Group {
             if store.state.homeState.homeRentors.count > 0 {
-                List {
-                    Section(header: self.headerView) {
-                        ForEach(store.state.homeState.homeRentors) { property in
+                VStack(spacing: 0) {
+                    HStack {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            TextField("search", text: $searchText, onEditingChanged: { _ in
+                                self.showCancelButton = true
+                            }, onCommit: {
+                                print("onCommit")
+                            }).foregroundColor(.primary)
+
+                            Button(action: {
+                                self.searchText = ""
+                            }) {
+                                Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                            }
+                        }.padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                        .foregroundColor(.secondary)
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10.0)
+                        if showCancelButton {
+                            Button("Cancel") {
+                                UIApplication.shared.endEditing(true)
+                                self.searchText = ""
+                                self.showCancelButton = false
+                            }
+                            .foregroundColor(Color(.systemBlue))
+                        }
+                    }.padding(.horizontal)
+                    .navigationBarHidden(showCancelButton)
+                    .background(Color.blue)
+                    List {
+                        ForEach(store.state.homeState.homeRentors.filter { ($0.name ?? "")
+                                    .hasPrefix(searchText) || searchText == ""}) { property in
                             ZStack {
                                 HomeRowView(rentor: property)
+                                    .padding(.top, 10)
+                                    .background(Color.pink)
+                                    
                                 NavigationLink(destination: HomeDetailView(with: property)) {
                                     EmptyView()
+                                        .background(Color.gray)
                                 }.frame(width: 0)
                                 .opacity(0)
-                                .buttonStyle(PlainButtonStyle())
+                                //.buttonStyle(PlainButtonStyle())
+                                .padding(.top, 10)
+                                .background(Color.black)
+                                
                             }.listRowInsets(EdgeInsets())
                             .padding(.all, 8)
                             .listRowBackground(Color.init("TableViewGray"))
+                            .background(Color.green)
+                            
                         }.onDelete(perform: onDelete)
-                    }
-                }.listStyle(GroupedListStyle())
-                
+                    }.listStyle(GroupedListStyle())
+                    .resignKeyboardOnDragGesture()
+                    .padding(.top, 10)
+                    .background(Color.yellow)
+                }.background(Color.red)
             } else {
                 Text("Please add a property... \(store.state.homeState.homeRentors.count)")
-            }
-        }
-    }
-    
-    var testButton: some View {
-        Group {
-            Button(action: {
-                print("do something crazy ")
-                store.dispatch(.homeAction(action: .fetch))
-                
-                store.dispatch(.settingsAction(action: .setHasLaunchedApp(status: false)))
-            }) {
-                Text("|¢|")
             }
         }
     }
@@ -76,9 +146,9 @@ extension HomeListView {
     var addButton: some View {
         Group {
             Button(action: { showingAddForm.toggle() }) {
-                Image(systemName: "plus.circle")
+                Image(systemName: "plus")
                     .imageScale(.large)
-                    .foregroundColor(Color.init("LightBlue"))
+                    .foregroundColor(Color.init("PrimaryViolet"))
             }.sheet(isPresented: $showingAddForm) {
                 SimmulatorView($showingAddForm)
                     .environmentObject(store)
@@ -90,5 +160,29 @@ extension HomeListView {
 struct HomeListView_Previews: PreviewProvider {
     static var previews: some View {
         HomeListView(onDelete: {_ in })
+    }
+}
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
     }
 }
