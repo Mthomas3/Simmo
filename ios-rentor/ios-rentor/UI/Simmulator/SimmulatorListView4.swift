10e36543cff17e7f769966b406b8b3c0f6e47fe3
mthomas3 <thomas.martins.dev@gmail.com>
2021-08-06 18:52:51 +0200

    [START] last page of first category 🎉

--- ios-rentor/ios-rentor/UI/Simmulator/SimmulatorListView4.swift
+++ ios-rentor/ios-rentor/UI/Simmulator/SimmulatorListView4.swift
@@ -9,6 +9,17 @@
 import SwiftUI
 import Combine
 
+struct LineTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding([.leading, .top, .bottom], 8)
+            .padding(.trailing, 12)
+            .frame(height: 56)
+            .cornerRadius(20)
+            .font(.system(size: 24))
+    }
+}
+
 struct SimmulatorListView4: View {
     
     @Binding var shouldPopToRootView: Bool
@@ -16,17 +27,32 @@ struct SimmulatorListView4: View {
     @State private var isCurrentSelected: Int?
     @State private var nextButton: Bool = false
     @State private var nexStep: Bool = false
+    @State internal var name: String = ""
     
+    let dataColor = (1...12).map { "Item \($0)" }
+    let dataImage = (1...20).map { "Item \($0)" }
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     func customSelectView() -> some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 25)
+            RoundedRectangle(cornerRadius: 12)
                 .frame(width: 66, height: 66)
                 .foregroundColor(.yellow)
         }
     }
     
     func customText() -> some View {
-        Text("YOLO THERE")
+        TextField("T2 dans le centre de Montpellier", text: $name)
+            .multilineTextAlignment(.leading)
+            .lineLimit(2)
+            .overlay(VStack{Divider().offset(x: 0, y: 15)})
+        
     }
  
     var body: some View {
@@ -42,18 +68,50 @@ struct SimmulatorListView4: View {
                         .padding(.leading, 24)
                         .padding(.trailing, 24)
                     
-                    HStack {
+                    HStack(alignment: .center) {
                         Spacer()
-                            .frame(width: 14)
                         customSelectView()
                         customText()
                         Spacer()
-                            .frame(width: 14)
-                    }
+                    }.padding([.leading, .trailing], 18)
+                    .padding(.bottom, 12)
                     
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(dataColor, id: \.self) { _ in
+                            customSelectView()
+                        }
+                    }.padding(.horizontal)
+                    .padding(.bottom, 24)
+                    
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(dataImage, id: \.self) { _ in
+                            customSelectView()
+                        }
+                    }.padding(.horizontal)
                 }
             }
         }.navigationBarTitle(Text(""), displayMode: .inline)
         .background(Color.init("BackgroundHome").edgesIgnoringSafeArea(.all))
+    }
+    
+    /*var body: some View {
+        //ContentView()
+    }*/
+}
+
+
+struct ContentView: View {
+    let data = (1...100).map { "Item \($0)" }
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    var body: some View {
+        ScrollView {
+            
+        }
     }
 }
