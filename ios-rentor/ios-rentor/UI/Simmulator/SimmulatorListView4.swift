34ac147bc5a9fd5430bb53b2d055190dae8aaa4b
mthomas3 <thomas.martins.dev@gmail.com>
2021-08-07 23:38:58 +0200

    ADD image on view

--- ios-rentor/ios-rentor/UI/Simmulator/SimmulatorListView4.swift
+++ ios-rentor/ios-rentor/UI/Simmulator/SimmulatorListView4.swift
@@ -29,7 +29,7 @@ struct SimmulatorListView4: View {
     @State private var nexStep: Bool = false
     @State internal var name: String = ""
     
-    let dataColor = (1...12).map { "Item \($0)" }
+    let dataColor: [Color] = (0...11).map { Color.init("Color\($0)") }
     let dataImage = (1...20).map { "Item \($0)" }
     
     let columns = [
@@ -39,11 +39,11 @@ struct SimmulatorListView4: View {
         GridItem(.flexible())
     ]
 
-    func customSelectView() -> some View {
+    func customSelectView(with color: Color) -> some View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .frame(width: 66, height: 66)
-                .foregroundColor(.yellow)
+                .foregroundColor(color)
         }
     }
     
@@ -52,7 +52,6 @@ struct SimmulatorListView4: View {
             .multilineTextAlignment(.leading)
             .lineLimit(2)
             .overlay(VStack{Divider().offset(x: 0, y: 15)})
-        
     }
  
     var body: some View {
@@ -70,48 +69,27 @@ struct SimmulatorListView4: View {
                     
                     HStack(alignment: .center) {
                         Spacer()
-                        customSelectView()
+                        customSelectView(with: Color.init("Color1"))
                         customText()
                         Spacer()
                     }.padding([.leading, .trailing], 18)
                     .padding(.bottom, 12)
                     
                     LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(dataColor, id: \.self) { _ in
-                            customSelectView()
+                        ForEach(dataColor, id: \.self) { color in
+                            customSelectView(with: color)
                         }
                     }.padding(.horizontal)
                     .padding(.bottom, 24)
                     
                     LazyVGrid(columns: columns, spacing: 20) {
                         ForEach(dataImage, id: \.self) { _ in
-                            customSelectView()
+                            customSelectView(with: Color.init("Color0"))
                         }
                     }.padding(.horizontal)
                 }
             }
         }.navigationBarTitle(Text(""), displayMode: .inline)
         .background(Color.init("BackgroundHome").edgesIgnoringSafeArea(.all))
-    }
-    
-    /*var body: some View {
-        //ContentView()
-    }*/
-}
-
-
-struct ContentView: View {
-    let data = (1...100).map { "Item \($0)" }
-
-    let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-
-    var body: some View {
-        ScrollView {
-            
-        }
     }
 }
